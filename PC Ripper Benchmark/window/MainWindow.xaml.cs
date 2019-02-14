1787925a252b395ad53409a53dc5e14dfdbd723f
REAPIX\Reapism <reapsprgm@gmail.com>
2019-02-13 21:29:49 -0500

    + MainWindow class.

---
+++ PC Ripper Benchmark/window/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PC_Ripper_Benchmark {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
