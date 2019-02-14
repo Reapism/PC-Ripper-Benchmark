442f6a942aa4f6a3afca86c5db5161d00bbb8099
REAPIX\Reapism <reapsprgm@gmail.com>
2019-02-13 21:22:03 -0500

    Add project files.

---
+++ PC Ripper Benchmark/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PC_Ripper_Benchmark {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
