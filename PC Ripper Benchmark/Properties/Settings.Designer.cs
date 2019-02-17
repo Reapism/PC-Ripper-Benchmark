fee18dd746f5a8f8e62bfd8f5378a6c2c9e65d3f
REAPIX\Reapism <reapsprgm@gmail.com>
2019-02-17 02:52:11 -0500

    Application settings to save per computer.
    (temporary until user accounts feature is added.)

--- PC Ripper Benchmark/Properties/Settings.Designer.cs
+++ PC Ripper Benchmark/Properties/Settings.Designer.cs
@@ -9,17 +9,77 @@
 //------------------------------------------------------------------------------
 
 namespace PC_Ripper_Benchmark.Properties {
-
-
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
+        
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong iter_successorship {
+            get {
+                return ((ulong)(this["iter_successorship"]));
+            }
+            set {
+                this["iter_successorship"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong iter_boolean {
+            get {
+                return ((ulong)(this["iter_boolean"]));
+            }
+            set {
+                this["iter_boolean"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong iter_queue {
+            get {
+                return ((ulong)(this["iter_queue"]));
+            }
+            set {
+                this["iter_queue"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong iter_linkedlist {
+            get {
+                return ((ulong)(this["iter_linkedlist"]));
+            }
+            set {
+                this["iter_linkedlist"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong iter_tree {
+            get {
+                return ((ulong)(this["iter_tree"]));
+            }
+            set {
+                this["iter_tree"] = value;
             }
         }
     }
