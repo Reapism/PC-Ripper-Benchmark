3ef713936fbbbf1fd8c07f890529c1329604d58d
REAPIX\Reapism <reapsprgm@gmail.com>
2019-02-23 00:55:52 -0500

    Represents the results for the CPU benchmark test. Either will implement IResults or extends abstract class results.

---
+++ PC Ripper Benchmark/util/CPUResults.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PC_Ripper_Benchmark.util {
+
+    /// <summary>
+    /// The <see cref="CPUResults"/> class.
+    /// <para></para>
+    /// Represents benchmarking results regarding
+    /// a particular test.
+    /// <para>Author: Anthony Jaghab (c), all rights reserved.</para>
+    /// </summary>
+
+    public class CPUResults : IResults
+    {
+        public Dictionary<string, TimeSpan> AveragePerTest => throw new NotImplementedException();
+
+        public Dictionary<string, TimeSpan> TimePerTest => throw new NotImplementedException();
+
+        public byte Score => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+    }
+}
