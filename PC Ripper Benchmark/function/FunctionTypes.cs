809e98d061512c173eff8c4fd4c62714d005b72f
REAPIX\Reapism <reapsprgm@gmail.com>
2019-02-23 00:51:20 -0500

    + FunctionTypes class
    Represents enums to be used throughout the functions.

---
+++ PC Ripper Benchmark/function/FunctionTypes.cs
@@ -0,0 +1,51 @@
+﻿namespace PC_Ripper_Benchmark.function {
+
+    /// <summary>
+    /// The <see cref="FunctionTypes"/> class.
+    /// <para>Contains public generalized functions
+    /// for all functions in the project, enums, and
+    /// data.</para>
+    /// </summary>
+
+    public class FunctionTypes {
+
+        /// <summary>
+        /// Determines which thread type a particular
+        /// action should be provoked in.
+        /// <para>SINGLE, SINGLEUI, MULTITHREADED</para>
+        /// </summary>
+
+        public enum ThreadType {
+
+            /// <summary>
+            /// Runs an action on the
+            /// same thread as the UI thread that spawned it.
+            /// <para>>> Note: This will freeze the program 
+            /// untilthe test is complete.</para>
+            /// </summary>
+            Single,
+
+            /// <summary>
+            /// Single threaded, runs an action on a new thread
+            /// separate the UI but not s
+            /// <para>>> Note: This will not freeze the
+            /// UI.</para>
+            /// </summary>
+            SingleUI,
+
+            /// <summary>
+            /// Runs an action split between N thread(s)
+            /// Contingent upon how many logical processor
+            /// (threads) are available on your machine. 
+            /// <para></para>Total threads will be (N + 1)
+            /// where 1 is the UI thread, n is # of threads on
+            /// this instances CPU.
+            /// <para>>> Note: This will not freeze the UI.</para>
+            /// </summary>
+            Multithreaded
+        }
+
+
+
+    }
+}
