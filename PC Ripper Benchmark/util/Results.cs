cfadc24fbc4680b59e89525eba0733f72872251c
REAPIX\Reapism <reapsprgm@gmail.com>
2019-02-25 01:58:51 -0500

    Removed IResuts, added abstract class Results
    Represents attributes and functions for all results for each component.

--- PC Ripper Benchmark/util/Results.cs
+++ PC Ripper Benchmark/util/Results.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 
 namespace PC_Ripper_Benchmark.util {
+
+    /// <summary>
+    /// The abstract <see cref="Results"/> class.
+    /// <para>Contains property signatures, and method signatures for
+    /// similar data for a particular component results.</para>
+    /// <para>Author: Anthony Jaghab (c), all rights reserved.</para>
+    /// </summary>
+
     public abstract class Results {
 
+        #region Abstract properties for a given component.
+
         /// <summary>
-        /// Represents a dictionary containing the name of the test,
+        /// Represents a <see cref="Tuple{T1, T2}"/> containing the name of the test,
         /// and the average time it took the test.
         /// </summary>
         public abstract Tuple<string, TimeSpan> AverageTest { get; }
@@ -13,7 +23,7 @@ namespace PC_Ripper_Benchmark.util {
         /// <summary>
         /// Represents the individual time for each test before the averaging.
         /// </summary>
-        public abstract Dictionary<string, TimeSpan> TestsCollection { get; }
+        public abstract List<TimeSpan> TestCollection { get; }
 
         /// <summary>
         /// Represents the score for this particular test.
@@ -25,31 +35,69 @@ namespace PC_Ripper_Benchmark.util {
         /// </summary>
         public abstract string Description { get; }
 
-        protected abstract void GenerateDescription();
-
-        protected abstract void GenerateScore();
+        /// <summary>
+        /// Represents the test name for this component.
+        /// </summary>
+        public abstract string TestName { get; }
 
         /// <summary>
-        /// Adds a particular test to the <see cref="TestsCollection"/>.
+        /// Gets the number of unique individual tests
+        /// for the component.
+        /// </summary>
+        public abstract byte UniqueTestCount { get; }
+
+        #endregion
+
+        #region Abstract methods.
+
+        /// <summary>
+        /// Generates a particular description based on a
+        /// given component test.
+        /// </summary>
+
+        protected abstract void GenerateDescription();
+
+        /// <summary>
+        /// Generates a score for a particular component.
+        /// </summary>
+
+        protected abstract byte GenerateScore();
+
+        /// <summary>
+        /// Adds a particular test to the <see cref="TestCollection"/>.
         /// </summary>
         /// <param name="name">The name of the function and test #.</param>
         /// <param name="duration">A <see cref="TimeSpan"/> that represents
         /// a duration for the test.</param>
-        
-        protected virtual void AddTest(string name, TimeSpan duration) {
-            TestsCollection.Add(name, duration);
+
+        protected virtual void AddTest(TimeSpan duration) {
+            this.TestCollection.Add(duration);
         }
 
-        protected virtual Tuple<string, TimeSpan> GenerateAverageTest(Dictionary<string, TimeSpan> testCollection) {
-            var newCollection = Tuple.Create(testCollection.Keys[], new TimeSpan());
-            int count = testCollection.Count;
+        /// <summary>
+        /// Generates a <see cref="Tuple"/> containing 
+        /// the Name and average test.
+        /// </summary>
+        /// <param name="testCollection"></param>
+        /// <returns>A Tuple containing a </returns>
+        /// <exception cref="DivideByZeroException">If testCollection is empty.</exception>
 
-            foreach (KeyValuePair<string, TimeSpan> kvp in testCollection) {
-                
+        protected virtual Tuple<string, TimeSpan> GenerateAverageTest(List<TimeSpan> testCollection) {
+            TimeSpan totalTime = new TimeSpan();
+
+            foreach (TimeSpan time in testCollection) {
+                totalTime = totalTime.Add(time);
             }
 
-            return newCollection;
+            if (testCollection.Count > 0) {
+                return Tuple.Create(
+                     this.TestName,
+                     new TimeSpan(totalTime.Ticks / testCollection.Count));
+            } else {
+                throw new DivideByZeroException();
+            }
         }
 
+        #endregion
     }
 }
