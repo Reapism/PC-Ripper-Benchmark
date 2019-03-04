960d634836b7b05138ecbe06f0a3d6916d5ac8c6
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-04 18:16:40 -0500

    Represents a virtual file in the Ripper.

---
+++ PC Ripper Benchmark/util/RipperFile.cs
@@ -0,0 +1,20 @@
+﻿namespace PC_Ripper_Benchmark.util {
+
+    /// <summary>
+    /// The <see cref="RipperFile"/> class.
+    /// <para>Represents a virtual file in 
+    /// memory. Stores the name of the file,
+    /// data of the file, size, and path.</para>
+    /// <para>Author: <see langword="Anthony Jaghab"/> (c),
+    /// all rights reserved.</para>
+    /// </summary>
+
+    public class RipperFile : RipperFolder {
+
+        private string fileName;
+        private string data;
+        private string size;
+        private RipperFolder path;
+
+    }
+}
