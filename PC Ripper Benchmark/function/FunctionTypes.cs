fe8149b2e2d2567336728f08b826eb6dc6d08d77
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-04 18:13:36 -0500

    Added comments, and an enum which represents all the test names for all the component benchmarking tests.

--- PC Ripper Benchmark/function/FunctionTypes.cs
+++ PC Ripper Benchmark/function/FunctionTypes.cs
@@ -1,10 +1,16 @@
-﻿namespace PC_Ripper_Benchmark.function {
+﻿using System.Collections.Generic;
+
+namespace PC_Ripper_Benchmark.function {
 
     /// <summary>
     /// The <see cref="FunctionTypes"/> class.
     /// <para>Contains public generalized functions
-    /// for all functions in the project, enums, and
-    /// data.</para>
+    /// for many functions in this project.</para>
+    /// <para>Includes <see langword="enums"/>, 
+    /// <see langword="functions"/>, and 
+    /// <see langword="data"/>.</para>
+    /// <para>Author: <see langword="Anthony Jaghab"/> (c),
+    /// all rights reserved.</para>
     /// </summary>
 
     public class FunctionTypes {
@@ -45,7 +51,86 @@
             Multithreaded
         }
 
+        /// <summary>
+        /// Represents all the test names for all the components.
+        /// </summary>
 
+        public enum TestName {
+            /// <summary>
+            /// The successorship test.
+            /// </summary>
+            CPUSuccessorship,
 
+            /// <summary>
+            /// The boolean logic.
+            /// </summary>
+            CPUBoolean,
+
+            /// <summary>
+            /// The <see cref="Queue{T}"/> test.
+            /// </summary>
+            CPUQueue,
+
+            /// <summary>
+            /// The <see cref="LinkedList{T}"/> test.
+            /// </summary>
+            CPULinkedList,
+
+            /// <summary>
+            /// The <see cref="SortedSet{T}"/> test.
+            /// </summary>
+            CPUTree,
+
+            /// <summary>
+            /// The folder matrix test.
+            /// </summary>
+            DiskFolderMatrix,
+
+            /// <summary>
+            /// The bulk-file test.
+            /// </summary>
+            DiskBulkFile,
+
+            /// <summary>
+            /// The read and write parsing test.
+            /// </summary>
+            DiskReadWriteParse,
+
+            /// <summary>
+            /// The combination of folder matrix and
+            /// bulk file.
+            /// </summary>
+            DiskRipper,
+
+            /// <summary>
+            /// The folder matrix test.
+            /// </summary>
+            RamVirtualFolderMatrix,
+
+            /// <summary>
+            /// The bulk-file test.
+            /// </summary>
+            RamVirtualBulkData,
+
+            /// <summary>
+            /// The read and write parsing test.
+            /// </summary>
+            RamReferenceDereferenceParse,
+
+            /// <summary>
+            /// The folder matrix test.
+            /// </summary>
+            GPUFolderMatrix,
+
+            /// <summary>
+            /// The bulk-file test.
+            /// </summary>
+            GPUBulkFile,
+
+            /// <summary>
+            /// The read and write parsing test.
+            /// </summary>
+            GPUReadWriteParse
+        };
     }
 }
