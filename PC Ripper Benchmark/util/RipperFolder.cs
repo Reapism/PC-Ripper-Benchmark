15c05064a870fa22aea3883b4e4e0e253f64648e
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-04 18:16:52 -0500

    Represents a virtual folder in the directory.

---
+++ PC Ripper Benchmark/util/RipperFolder.cs
@@ -0,0 +1,18 @@
+﻿namespace PC_Ripper_Benchmark.util {
+
+    /// <summary>
+    /// The <see cref="RipperFolder"/> class.
+    /// <para>Represents a virtual folder in 
+    /// memory. Stores the name of the directory
+    /// and it's path on the user.</para>
+    /// <para>Author: <see langword="Anthony Jaghab"/> (c),
+    /// all rights reserved.</para>
+    /// </summary>
+
+    public class RipperFolder {
+
+        private string folderName;
+        private string path;
+
+    }
+}
