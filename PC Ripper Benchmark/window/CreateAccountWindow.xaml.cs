45dfbc39ab48f5111611d199a63a6d16698dc424
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-06 21:14:38 -0500

    cleaned using statements.

--- PC Ripper Benchmark/window/CreateAccountWindow.xaml.cs
+++ PC Ripper Benchmark/window/CreateAccountWindow.xaml.cs
@@ -1,23 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Text.RegularExpressions;
-using System.Windows.Media.Animation;
 
-namespace PC_Ripper_Benchmark
-{
+namespace PC_Ripper_Benchmark {
     /// <summary>
     /// The <see cref="CreateAccountWindow"/> class.
     /// <para></para>
