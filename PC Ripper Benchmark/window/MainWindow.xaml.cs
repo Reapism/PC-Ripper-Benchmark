88cf81d29b67fbb32cddeefd566aeaf4051ee6ff
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-10 18:15:42 -0400

    add tab control in encapsualting more grids. formatting is good.

--- PC Ripper Benchmark/window/MainWindow.xaml.cs
+++ PC Ripper Benchmark/window/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace PC_Ripper_Benchmark.window {
@@ -10,31 +11,77 @@ namespace PC_Ripper_Benchmark.window {
 
     public partial class MainWindow : Window {
 
+        #region Instance member(s), and enum(s).
+
+        /// <summary>
+        /// The <see cref="Tab"/> enum.
+        /// Represents all the tabs needed.
+        /// </summary>
+
+        public enum Tab {
+            /// <summary>
+            /// The cpu tab.
+            /// </summary>
+            CPU,
+
+            /// <summary>
+            /// The disk tab.
+            /// </summary>
+            DISK,
+
+            /// <summary>
+            /// The ram tab.
+            /// </summary>
+            RAM,
+
+            /// <summary>
+            /// The gpu tab.
+            /// </summary>
+            GPU
+        }
+
+        #endregion
+
+        #region Constructor(s) and method(s).
+
         /// <summary>
         /// Default constructor for the <see cref="MainWindow"/>.
         /// </summary>
 
         public MainWindow() {
             InitializeComponent();
+            Style s = new Style();
+            s.Setters.Add(new Setter(UIElement.VisibilityProperty, Visibility.Collapsed));
+            tabComponents.ItemContainerStyle = s;
+            tabComponents.SelectedIndex = 0;
         }
+
+        private void ShowCPUWindow() {
+            tabComponents.SelectedIndex = 1;
+        }
+
+        #endregion
+
+        #region Event(s) and event handler(s).
 
         private void BtnTemp_Click(object sender, RoutedEventArgs e) {
             new QuestionaireWindow().Show();
         }
 
         private void BtnCPU_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e) {
-            btnCPU.BorderThickness = new Thickness(3.0);
-            btnCPU.BorderBrush = Brushes.Salmon;
+            btnCPU.Foreground = Brushes.Salmon;
+
         }
 
         private void BtnCPU_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e) {
-            btnCPU.BorderThickness = new Thickness(3.0);
-            btnCPU.BorderBrush = Brushes.Transparent;
+            btnCPU.Foreground = Brushes.Black;
         }
 
         private void BtnCPU_Click(object sender, RoutedEventArgs e) {
-            btnCPU.BorderThickness = new Thickness(3.0);
-            btnCPU.BorderBrush = Brushes.Salmon;
+            ShowCPUWindow();
         }
+
+        #endregion
+
     }
 }
