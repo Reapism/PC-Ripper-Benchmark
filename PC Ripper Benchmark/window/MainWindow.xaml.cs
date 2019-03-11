64154d763b86fc6e25e607bf401872834e55c421
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-11 01:11:32 -0400

    Reupdated MainWindow

--- PC Ripper Benchmark/window/MainWindow.xaml.cs
+++ PC Ripper Benchmark/window/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
+
+using static PC_Ripper_Benchmark.function.RipperTypes;
 
 namespace PC_Ripper_Benchmark.window {
 
@@ -11,7 +12,7 @@ namespace PC_Ripper_Benchmark.window {
 
     public partial class MainWindow : Window {
 
-        #region Instance member(s), and enum(s).
+        #region Instance member(s), and enum(s).        
 
         #endregion
 
@@ -25,12 +26,43 @@ namespace PC_Ripper_Benchmark.window {
             InitializeComponent();
             Style s = new Style();
             s.Setters.Add(new Setter(UIElement.VisibilityProperty, Visibility.Collapsed));
-            tabComponents.ItemContainerStyle = s;
-            tabComponents.SelectedIndex = 0;
+            this.tabComponents.ItemContainerStyle = s;
+            this.tabComponents.SelectedIndex = 0;
         }
 
-        private void ShowCPUWindow() {
-            tabComponents.SelectedIndex = 1;
+        private void ShowTabWindow(Tab theTab) {
+
+            switch (theTab) {
+                case Tab.WELCOME: {
+                    this.tabComponents.SelectedIndex = (int)Tab.WELCOME;
+                    return;
+                }
+
+                case Tab.CPU: {
+                    this.tabComponents.SelectedIndex = (int)Tab.CPU;
+                    return;
+                }
+
+                case Tab.DISK: {
+                    this.tabComponents.SelectedIndex = (int)Tab.DISK;
+                    return;
+                }
+
+                case Tab.RAM: {
+                    this.tabComponents.SelectedIndex = (int)Tab.RAM;
+                    return;
+                }
+
+                case Tab.GPU: {
+                    this.tabComponents.SelectedIndex = (int)Tab.GPU;
+                    return;
+                }
+
+                case Tab.SETTINGS: {
+                    this.tabComponents.SelectedIndex = (int)Tab.SETTINGS;
+                    return;
+                }
+            }
         }
 
         #endregion
@@ -42,19 +74,42 @@ namespace PC_Ripper_Benchmark.window {
         }
 
         private void BtnCPU_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e) {
-            btnCPU.Foreground = Brushes.Salmon;
-
+            this.btnCPU.Foreground = Brushes.Salmon;
         }
 
         private void BtnCPU_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e) {
-            btnCPU.Foreground = Brushes.Black;
+            this.btnCPU.Foreground = Brushes.Black;
         }
 
         private void BtnCPU_Click(object sender, RoutedEventArgs e) {
-            ShowCPUWindow();
+            ShowTabWindow(Tab.CPU);
+        }
+
+        private void GrdCPUTests_SizeChanged(object sender, SizeChangedEventArgs e) {
+            //double widthDifference = System.Math.Abs(grdCPUTests.ActualWidth - e.NewSize.Width - e.PreviousSize.Width);
+
+            //this.imgSuccessorship.Margin = new Thickness(this.imgSuccessorship.Margin.Left + widthDifference,
+            //    this.imgSuccessorship.Margin.Top, this.imgSuccessorship.Margin.Right,
+            //    this.imgSuccessorship.Margin.Bottom);
+
+            //this.imgBoolean.Margin = new Thickness(this.imgBoolean.Margin.Left + widthDifference,
+            //    this.imgBoolean.Margin.Top, this.imgBoolean.Margin.Right,
+            //    this.imgBoolean.Margin.Bottom);
+
+            //this.imgQueue.Margin = new Thickness(this.imgQueue.Margin.Left + widthDifference,
+            //    this.imgQueue.Margin.Top, this.imgQueue.Margin.Right,
+            //    this.imgQueue.Margin.Bottom);
+
+            //this.imgLinkedList.Margin = new Thickness(this.imgLinkedList.Margin.Left + widthDifference,
+            //    this.imgLinkedList.Margin.Top, this.imgLinkedList.Margin.Right,
+            //    this.imgLinkedList.Margin.Bottom);
+
+            //this.imgTree.Margin = new Thickness(this.imgTree.Margin.Left + widthDifference,
+            //    this.imgTree.Margin.Top, this.imgTree.Margin.Right,
+            //    this.imgTree.Margin.Bottom);
+
         }
 
         #endregion
-
     }
 }
