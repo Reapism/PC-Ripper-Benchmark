1b1381f7d9ef07d0a846926b57c60369c4207d3b
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-13 00:22:16 -0400

    Added constructors, and huge comment base for parameterized ctor.

--- PC Ripper Benchmark/util/RipperFile.cs
+++ PC Ripper Benchmark/util/RipperFile.cs
@@ -11,13 +11,45 @@
 
     public class RipperFile {
 
+        #region Instance members (fields).
+
         private string fileName;
         private string data;
         private long size;
         private RipperFolder parentDir;
 
-        public RipperFile(string fileName, string data, long size, RipperFolder parentDir) {
+        #endregion
 
+        #region Constructor(s).
+
+        /// <summary>
+        /// Default constructor for creating a <see cref="RipperFile"/>.
+        /// <para>It's best to use the parameterized constructor.</para>
+        /// </summary>
+
+        public RipperFile() : this("", "", 0, null) { }
+
+        /// <summary>
+        /// Parameterized constructor for creating a 
+        /// <see cref="RipperFile"/>.
+        /// </summary>
+        /// <param name="fileName">The name of the 
+        /// <see cref="RipperFile"/>. <para>i.e. 
+        /// "file{ <see langword="N"/> }"
+        /// where <see langword="N"/> represents a number.</para>
+        /// </param>
+        /// <param name="data">The contents of the
+        /// <see cref="RipperFile"/>.</param>
+        /// <param name="size">The size of the <see cref="RipperFile"/>.</param>
+        /// <param name="parentDir">The parent directory of this <see cref="RipperFile"/>.</param>
+
+        public RipperFile(string fileName, string data, long size, RipperFolder parentDir) {
+            this.fileName = fileName;
+            this.data = data;
+            this.size = size;
+            this.parentDir = parentDir;
         }
+
+        #endregion
     }
 }
