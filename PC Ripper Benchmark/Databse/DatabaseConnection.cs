76472134b315f22e2942d9dcec25f05ea4d83b77
David Hartglass <davidhartglass@gmail.com>
2019-03-18 12:16:34 -0400

    Database folder and class

---
+++ PC Ripper Benchmark/Databse/DatabaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_Ripper_Benchmark.Databse
+{
+    class DatabaseConnection
+    {
+    }
+}
