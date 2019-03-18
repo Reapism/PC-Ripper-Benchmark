5439f0c69ffca020944c00aa30918eb8d898eba9
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-18 15:26:08 -0400

    .meta

--- PC Ripper Benchmark/Database/DatabaseConnection.cs
+++ PC Ripper Benchmark/Database/DatabaseConnection.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PC_Ripper_Benchmark.Databse
-{
-    class DatabaseConnection
-    {
+﻿namespace PC_Ripper_Benchmark.Databse {
+    class DatabaseConnection {
     }
 }
