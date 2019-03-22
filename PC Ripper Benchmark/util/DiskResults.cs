7c552f20d183a5a2d51048c72a2918ed0c89b006
REAPIX\Reapism <reapsprgm@gmail.com>
2019-03-22 13:01:08 -0400

    parital implementation of disk results

--- PC Ripper Benchmark/util/DiskResults.cs
+++ PC Ripper Benchmark/util/DiskResults.cs
@@ -1,6 +1,7 @@
 ﻿using PC_Ripper_Benchmark.function;
 using System;
 using System.Collections.Generic;
+using static PC_Ripper_Benchmark.function.RipperTypes;
 
 namespace PC_Ripper_Benchmark.util {
 
@@ -16,21 +17,44 @@ namespace PC_Ripper_Benchmark.util {
 
     public class DiskResults : Results {
 
-        public DiskResults() {
+        private readonly RipperSettings rs;
+        private const byte uniqueTestCount = 4;
 
+        /// <summary>
+        /// Constructs <see cref="DiskResults"/> with
+        /// a <see cref="RipperSettings"/> instance.
+        /// </summary>
+        /// <param name="rs">Takes in an initial <see cref="RipperSettings"/>
+        /// but is marked <see langword="readonly"/> internally.</param>
+
+        public DiskResults(RipperSettings rs) {
+            this.TestCollection = new List<TimeSpan>();
+            this.rs = rs;
         }
 
-        public override List<TimeSpan> TestCollection => throw new NotImplementedException();
+        /// <summary>
+        /// Represents all the timespans for the DISK tests.
+        /// </summary>
+        
+        public override List<TimeSpan> TestCollection { get; }
 
-        public override byte Score => throw new NotImplementedException();
+        /// <summary>
+        /// Represents the score for the test.
+        /// </summary>
 
-        public override string Description => throw new NotImplementedException();
+        public override byte Score => GenerateScore();
 
-        public override byte UniqueTestCount => throw new NotImplementedException();
+        /// <summary>
+        /// Represents the description for test.
+        /// </summary>
 
-        protected override Tuple<string, TimeSpan> GenerateAverageTest(List<TimeSpan> testCollection, RipperTypes.TestName testName) {
-            throw new NotImplementedException();
-        }
+        public override string Description => GenerateDescription();
+
+        /// <summary>
+        /// The number of different tests for the DISK component.
+        /// </summary>
+
+        public override byte UniqueTestCount => uniqueTestCount;
 
         protected override string GenerateDescription() {
             throw new NotImplementedException();
@@ -38,6 +62,78 @@ namespace PC_Ripper_Benchmark.util {
 
         protected override byte GenerateScore() {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="Tuple{T1, T2}"/> containing
+        /// the name and average of the specific test under the
+        /// <see langword="DISK component"/>.
+        /// </summary>
+        /// <param name="testCollection">The total collection of <see cref="TimeSpan"/>(s)
+        /// for the component.</param>
+        /// <param name="theTest">The test represented by <see cref="TestName"/>.</param>
+        /// <returns></returns>
+
+        protected override Tuple<string, TimeSpan> GenerateAverageTest(List<TimeSpan> testCollection, TestName theTest) {
+            Tuple<string, TimeSpan> averageTest;
+
+            switch (theTest) {
+
+                case TestName.DISKFolderMatrix: {
+                    TimeSpan totalTime = new TimeSpan();
+
+                    for (byte b = 0; b < this.rs.IterationsPerDiskTest; b++) {
+                        totalTime = totalTime.Add(this.TestCollection[b]);
+                    }
+
+                    TimeSpan average = AverageTimespan(ref totalTime, this.rs.IterationsPerDiskTest);
+
+                    averageTest = Tuple.Create(GetTestName(theTest), average);
+                    break;
+                }
+
+                case TestName.DISKBulkFile: {
+                    TimeSpan totalTime = new TimeSpan();
+
+                    for (byte b = 0; b < this.rs.IterationsPerDiskTest; b++) {
+                        totalTime = totalTime.Add(this.TestCollection[b + (this.rs.IterationsPerDiskTest * 1)]);
+                    }
+
+                    TimeSpan average = AverageTimespan(ref totalTime, this.rs.IterationsPerDiskTest);
+                    averageTest = Tuple.Create(GetTestName(theTest), average);
+                    break;
+                }
+
+                case TestName.DISKReadWriteParse: {
+                    TimeSpan totalTime = new TimeSpan();
+
+                    for (byte b = 0; b < this.rs.IterationsPerDiskTest; b++) {
+                        totalTime = totalTime.Add(this.TestCollection[b + (this.rs.IterationsPerDiskTest * 2)]);
+                    }
+
+                    TimeSpan average = AverageTimespan(ref totalTime, this.rs.IterationsPerDiskTest);
+                    averageTest = Tuple.Create(GetTestName(theTest), average);
+                    break;
+                }
+
+                case TestName.DISKRipper: {
+                    TimeSpan totalTime = new TimeSpan();
+
+                    for (byte b = 0; b < this.rs.IterationsPerDiskTest; b++) {
+                        totalTime = totalTime.Add(this.TestCollection[b + (this.rs.IterationsPerDiskTest * 3)]);
+                    }
+
+                    TimeSpan average = AverageTimespan(ref totalTime, this.rs.IterationsPerDiskTest);
+                    averageTest = Tuple.Create(GetTestName(theTest), average);
+                    break;
+                }
+
+                default: {
+                    return Tuple.Create("**UnknownTest**", new TimeSpan());
+                }
+            }
+
+            return averageTest;
         }
     }
 }
