cdb0c087e8e9789f859528beb5c9b372f52b48da
David Hartglass <davidhartglass@gmail.com>
2019-04-08 13:38:39 -0400

    Fixed IsInternetAvailable bool

--- PC Ripper Benchmark/function/SystemSettings.cs
+++ PC Ripper Benchmark/function/SystemSettings.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace PC_Ripper_Benchmark.function {
 
@@ -18,8 +21,22 @@ namespace PC_Ripper_Benchmark.function {
         /// </summary>
         /// <returns></returns>
 
-        public static bool IsInternetAvailable() =>
-            NetworkInterface.GetIsNetworkAvailable();
+        public static bool IsInternetAvailable()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://clients3.google.com/generate_204"))
+                {
+                    return true;
+                }
+            }
+            catch (System.Exception)
+            {
+                System.Windows.MessageBox.Show("No internet connection!", "No Internet", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+        }
 
     }
 }
