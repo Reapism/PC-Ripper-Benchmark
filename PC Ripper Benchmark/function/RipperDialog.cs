14963616b4faddcaca05b1e212e4193b80b1c555
REAPIX\Reapism <reapsprgm@gmail.com>
2019-04-19 12:28:27 -0400

    changes

---
+++ PC Ripper Benchmark/function/RipperDialog.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PC_Ripper_Benchmark.function {
+
+    /// <summary>
+    /// The <see cref="RipperDialog"/> class.
+    /// <para> </para>
+    /// Represents all the functions for 
+    /// creating dialog boxes.
+    /// <para>Author: <see langword="Anthony Jaghab"/> (c),
+    /// all rights reserved.</para>
+    /// </summary>
+
+    public class RipperDialog {
+
+        
+
+        private static System.Windows.Forms.DialogResult InputBoxHelper() {
+
+        }
+
+        private static LinearGradientBrush CreateLinearGradient(Brushes color1, Brushes color2, params Point[] points) {
+            LinearGradientBrush linear = new LinearGradientBrush {
+                StartPoint = new Point(0, 0),
+                EndPoint = new Point(1, 1)
+            };
+            int numPoints = points.Length;
+
+            foreach (Point p in points) {
+            linear.GradientStops.Add(new GradientStop() { Color = (Color)color1, Offset = 0.0 });
+            linear.GradientStops.Add(new GradientStop() { Color = color2, Offset = 0.25 });
+            linear.GradientStops.Add(new GradientStop() { Color = Colors.Blue, Offset = 0.75 });
+            linear.GradientStops.Add(new GradientStop() { Color = Colors.Green, Offset = 1.0 });
+
+            }
+
+        }
+
+        public static System.Windows.Forms.DialogResult InputBox(string message, out string inputText, string caption = "Ripper Dialog") {
+            Window w = new Window {
+                WindowStyle = WindowStyle.None,
+                Content = caption,
+                Background = GradientBrush
+            };
+
+            TextBox textBox = new System.Windows.Controls.TextBox {
+                Text = message,
+                Background = Brushes.Transparent,
+                Foreground = Brushes.White,
+
+                
+                
+            };
+        }
+
+    }
+}
