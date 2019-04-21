20cbb2215e71bdc8d41d4ad372031e03a334cb91
David Hartglass <davidhartglass@gmail.com>
2019-04-21 12:10:16 -0400

    Added static method to UserData to capitalize the first letter of a name (string)

--- PC Ripper Benchmark/util/UserData.cs
+++ PC Ripper Benchmark/util/UserData.cs
@@ -243,6 +243,17 @@
             return name;
         }
 
+
+        public static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
         #endregion
 
         #region Properties
