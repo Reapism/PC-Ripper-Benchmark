b097d7dd5a30a5ebc377cf6a7b7b09c7158fcf64
REAPIX\Reapism <reapsprgm@gmail.com>
2019-04-23 01:06:12 -0400

    represents a base computer spec

---
+++ PC Ripper Benchmark/util/BaseComputerSpec.cs
@@ -0,0 +1,16 @@
+﻿namespace PC_Ripper_Benchmark.util {
+
+    public class BaseComputerSpec {
+
+        public BaseComputerSpec() {
+
+        }
+
+        public string CPUName { get => "Intel(R) Core(TM) i7-6700K CPU @ 4.00GHz"; }
+
+        public string RAMName { get => "Intel(R) Core(TM) i7-6700K CPU @ 4.00GHz"; }
+
+        public string DISKName { get => "Intel(R) Core(TM) i7-6700K CPU @ 4.00GHz"; }
+
+    }
+}
