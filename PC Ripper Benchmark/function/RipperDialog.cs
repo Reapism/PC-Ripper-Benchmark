7534f1808b6a22f91ee37d687f698286558e8016
REAPIX\Reapism <reapsprgm@gmail.com>
2019-04-22 20:07:37 -0400

    Uses InputDialog from VB.

--- PC Ripper Benchmark/function/RipperDialog.cs
+++ PC Ripper Benchmark/function/RipperDialog.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
+﻿using Microsoft.VisualBasic;
 
 namespace PC_Ripper_Benchmark.function {
 
@@ -15,68 +13,73 @@ namespace PC_Ripper_Benchmark.function {
 
     public class RipperDialog {
 
+        /// <summary>
+        /// Displays and returns the string in the <see cref="Interaction.InputBox"/>.
+        /// </summary>
+        /// <param name="prompt">The prompt of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="title">The title of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="defaultResponse">The default response of the <see cref="Interaction.InputBox"/>.</param>
+        /// <returns></returns>
 
-
-        private static System.Windows.Forms.DialogResult InputBoxHelper() {
-            return System.Windows.Forms.DialogResult.OK;
+        public static string InputBox(string prompt, string title, string defaultResponse) {
+            return Interaction.InputBox(prompt, title, defaultResponse);
         }
 
-        private static LinearGradientBrush CreateLinearGradient(Color color1, Color color2, params Point[] points) {
-            LinearGradientBrush linear = new LinearGradientBrush {
-                StartPoint = new Point(0, 0),
-                EndPoint = new Point(1, 1)
-            };
-            int numPoints = points.Length;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prompt">The prompt of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="title">The title of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="defaultResponse">The default response of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="output">The output of the ulong.</param>
+        /// <returns></returns>
 
-            foreach (Point p in points) {
-                linear.GradientStops.Add(new GradientStop() { Color = color1, Offset = 0.0 });
-                linear.GradientStops.Add(new GradientStop() { Color = color2, Offset = 0.25 });
-
+        public static bool InputBox(string prompt, string title, string defaultResponse, out ulong output) {
+            try {
+                bool b = ulong.TryParse(Interaction.InputBox(prompt, title, defaultResponse), out output);
+                return b;
+            } catch {
+                output = 0;
+                return false;
             }
-            return linear;
         }
 
-        public static bool? InputBox(string message, out string inputText, string caption = "Ripper Dialog") {
-            Window w = new Window {
-                WindowStyle = WindowStyle.None,
-                Content = caption,
-                Background = CreateLinearGradient(Colors.AliceBlue, Colors.AntiqueWhite, new Point(0.0, 1.0)),
-                DialogResult = false
-            };
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prompt">The prompt of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="title">The title of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="defaultResponse">The default response of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="output">The output of the byte.</param>
+        /// <returns></returns>
 
-            StackPanel stk = new StackPanel {
-
-            };
-
-            TextBox textBox = new System.Windows.Controls.TextBox {
-                Text = message,
-                Background = Brushes.Transparent,
-                Foreground = Brushes.White,
-            };
-
-            Button ok = new Button {
-                Content = "Okay",
-                Opacity = 0.0,
-                Foreground = Brushes.White,
-                BorderBrush = Brushes.White,
-                BorderThickness = new Thickness(1)
-            };
-
-            Button cancel = new Button {
-                Content = "Cancel",
-                Opacity = 0.0,
-                Foreground = Brushes.White,
-                BorderBrush = Brushes.White,
-                BorderThickness = new Thickness(1)
-            };
-
-            stk.Children.Add(textBox);
-            stk.Children.Add(ok);
-            stk.Children.Add(cancel);
-
-            inputText = "";
-            return w.ShowDialog();
+        public static bool InputBox(string prompt, string title, string defaultResponse, out byte output) {
+            try {
+                bool b = byte.TryParse(Interaction.InputBox(prompt, title, defaultResponse), out output);
+                return b;
+            } catch {
+                output = 0;
+                return false;
+            }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prompt">The prompt of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="title">The title of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="defaultResponse">The default response of the <see cref="Interaction.InputBox"/>.</param>
+        /// <param name="output">The output of the int.</param>
+        /// <returns></returns>
+
+        public static bool InputBox(string prompt, string title, string defaultResponse, out int output) {
+            try {
+                bool b = int.TryParse(Interaction.InputBox(prompt, title, defaultResponse), out output);
+                return b;
+            } catch {
+                output = 0;
+                return false;
+            }
+        }
     }
 }
